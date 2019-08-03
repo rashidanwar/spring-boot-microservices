a428bbcd388ba0fa179bc2064c23ed376cf638fe
rashid09 <rashid.anwar.it09@gmail.com>
2019-08-03 15:35:00 +0530

    added ribbon properties

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/resource/MovieResource.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resource/MovieResource.java
@@ -1,29 +1,24 @@
 package io.javabrains.movieinfoservice.resource;
 
 import io.javabrains.movieinfoservice.model.Movie;
-import io.javabrains.movieinfoservice.model.MovieSummary;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import io.javabrains.movieinfoservice.service.MovieInfoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
-    @Value("${api.key}")
-    private String apiKey;
+    private MovieInfoService movieInfoService;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    public MovieResource(MovieInfoService movieInfoService) {
+        this.movieInfoService = movieInfoService;
+    }
 
     @GetMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable String movieId) {
-        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
-
+        return movieInfoService.getMovieInfo(movieId);
     }
 }
